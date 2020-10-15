af8d60cc694e31ecb73756e35a327aeb3e6a2e2c
Anthony Ikeda <anthony.ikeda@gmail.com>
2020-10-15 14:31:38 -0700

    Should fix the build

--- src/app/_service/pet-food.service.spec.ts
+++ src/app/_service/pet-food.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PetFoodService } from './pet-food.service';
@@ -6,7 +7,9 @@ describe('PetFoodService', () => {
   let service: PetFoodService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(PetFoodService);
   });
 
--- src/app/_service/pet-food.service.ts
+++ src/app/_service/pet-food.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PetFood } from './pet.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,7 @@ export class PetFoodService {
   constructor(private http: HttpClient) { }
 
   getPetFood(): Observable<PetFood[]> {
-    return new Observable(()
-
-    )
+    return null;
   }
 
 }
